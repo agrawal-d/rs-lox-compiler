dd2cf29d8e9949ec07090bdcb18ce7903b7c693c
Divyanshu Agrawal <agrawal-d@outlook.com>
2024-05-07 21:53:41 +0530

    Fibbonacchi working.

--- native/src/main.rs
+++ native/src/main.rs
@@ -26,19 +26,19 @@ fn help(args: &Vec<String>) {
 fn main() {
     init(print, println);
 
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        help(&args);
-        std::process::exit(1);
-    }
+    // let args: Vec<String> = std::env::args().collect();
+    // if args.len() != 2 {
+    //     help(&args);
+    //     std::process::exit(1);
+    // }
 
-    if args[1] == "-h" || args[1] == "--help" {
-        help(&args);
-        std::process::exit(0);
-    }
+    // if args[1] == "-h" || args[1] == "--help" {
+    //     help(&args);
+    //     std::process::exit(0);
+    // }
 
-    let path = &args[1];
-    let input = std::fs::read_to_string(path).expect("Failed to read file");
+    // let path = &args[1];
+    let input = std::fs::read_to_string("/home/divyanshu/p/web-compiler/native/code.cs").expect("Failed to read file");
 
     run_code(&input);
 }
